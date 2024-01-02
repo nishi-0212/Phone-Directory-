b052f001374ebbbfc4ae0ed88bf8cea4f75d5480
Nishi Vishwakarma <132808787+nishi-0212@users.noreply.github.com>
2024-01-02 20:03:21 +0530

    phonebook.c

--- phonebook.c
+++ phonebook.c
@@ -447,10 +447,7 @@ void end()
     system("cls");
 
     printf(">>> Phone Book in C <<< \n\n");
-    printf("> Creator: @Alkaison (Ganesh Mourya) \n\n");
-    printf("> Github: https://github.com/Alkaison \n");
-    printf("> Twitter: https://twitter.com/Alkaison \n");
-    printf("> LinkedIn: https://www.linkedin.com/in/Alkaison \n\n");
+    printf("> Creator: Nishi Vishwakarma \n\n");
     printf("Press any key to exit. \n");
 
     getch();
